154d7749ceb84e23873d0963687048cd3cad1450
Maikel van Veen <maikel.vanveen@coolblue.nl>
2022-07-27 11:42:15 +0200

    Correct solution 3

--- solutions/exercise_3_solution.ts
+++ solutions/exercise_3_solution.ts
@@ -1,15 +1,35 @@
 /*
  * ---------------------------------------------------------------------------------------------------- 
  * Exercise 3/5: Union Types & casting
- * Correctly type the argument type (Record<string, unknown>) of the function below without using the any or unknown keywords.
- * You will see that you probably have to correct the logic in isAnimal to make TypeScript
- * understand what you are trying to do.
+ * Type the argument type (Record<string, unknown>) of the function without using the any or unknown keywords.
+ * You probably will have to correct the logic in isAnimal to make TypeScript understand what you are trying to do.
  * 
  * FYI: Use type casting very wisely, since you are overruling TypeScript.
  * ----------------------------------------------------------------------------------------------------
  */
 
-const _person = {
+type AddressType = {
+    houseNumber: number;
+    street: string;
+    zipCode: string;
+}
+
+type AnimalType = {
+    name: string;
+    age: number;
+    children: string[];
+    address: AddressType;
+};
+
+type PersonType = {
+    name: string;
+    age: number;
+    children: string[];
+    occupation: string;
+    address: AddressType;
+};
+
+const person = {
     name: 'John',
     age: 67,
     children: [
@@ -24,7 +44,7 @@ const _person = {
     }
 }
 
-const _animal = {
+const animal = {
     name: 'Felix',
     age: 10,
     children: [
@@ -38,30 +58,9 @@ const _animal = {
     }
 }
 
-type _AddressType2 = {
-    houseNumber: number;
-    street: string;
-    zipCode: string;
+function isAnimal(animalOrPerson: AnimalType | PersonType): boolean {
+    return !Boolean((animalOrPerson as PersonType).occupation);
 }
 
-type _AnimalType = {
-    name: string;
-    age: number;
-    children: string[];
-    address: _AddressType2;
-};
-
-type _PersonType = {
-    name: string;
-    age: number;
-    children: string[];
-    occupation: string;
-    address: _AddressType2;
-};
-
-function _isAnimal(animalOrPerson: _AnimalType | _PersonType): boolean {
-    return !Boolean((animalOrPerson as _PersonType).occupation);
-}
-
-_isAnimal(_person); // Should return false
-_isAnimal(_animal); // Should return true
+console.log(isAnimal(person)); // Should be false
+console.log(isAnimal(animal)); // Should be true
